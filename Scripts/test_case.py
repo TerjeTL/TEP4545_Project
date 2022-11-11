c0174349760ea1ed9eed6dbda5e1fe401a68388d
TerjeTL <43807859+TerjeTL@users.noreply.github.com>
2022-11-11 14:12:20 +0100

    Update test_case.py
    automated cases

--- Scripts/test_case.py
+++ Scripts/test_case.py
@@ -11,17 +11,29 @@ NUM_CPU = 6
 
 ############
 
-# Fully clean up before making a new run
-rc = call([CASE_DIR + "cleanAll.sh", "-y"])
+class Case:
+    def __init__(self, case_name, length_to_thickness_ratio, cell_density, wall_distance):
+        self.case_name = case_name
+        self.length_to_thickness_ratio = length_to_thickness_ratio
+        self.cell_density = cell_density
+        self.wall_distance = wall_distance
 
-# Run the test case
-rc += call([CASE_DIR + "runAll.sh", "-y", "-p", str(NUM_CPU)])
+    def run(self):
+         # Fully clean up before making a new run
+        rc = call([CASE_DIR + "cleanAll.sh", "-y"])
 
-# Export data from run
-name = "test_data_export.csv"
-rc += call([CASE_DIR + "exportResults.sh", "-n", name])
+        # Run the test case
+        rc += call([CASE_DIR + "runAll.sh", "-y", "-p", str(NUM_CPU)])
 
+        rc += call([CASE_DIR + "exportResults.sh", "-n", self.case_name])
 
-print("done from python!")
+        return rc
 
-print("exit code:", rc)
+def main():
+    for i in range(5):
+        case_name = "test_case" + str(i)
+        test_case = Case(case_name, 5, 100, 10)
+        test_case.run()
+
+if __name__ == '__main__':
+    main()
