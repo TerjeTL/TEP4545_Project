88a6d9a46eb7f9c58896509eee93b533f90020ba
Terje Lindtveit <terjetlindtveit@gmail.com>
2022-11-11 13:25:24 +0100

    case object

--- Scripts/test_case.py
+++ Scripts/test_case.py
@@ -11,17 +11,26 @@ NUM_CPU = 6
 
 ############
 
-# Fully clean up before making a new run
-rc = call([CASE_DIR + "cleanAll.sh", "-y"])
+def Case:
+    def __init__(self, case_name, length_to_thickness_ratio, cell_density):
+        self.case_name = case_name
+        self.length_to_thickness_ratio = length_to_thickness_ratio
+        self.cell_density = cell_density
 
-# Run the test case
-rc += call([CASE_DIR + "runAll.sh", "-y", "-p", str(NUM_CPU)])
+    def run(self):
+         # Fully clean up before making a new run
+        rc = call([CASE_DIR + "cleanAll.sh", "-y"])
 
-# Export data from run
-name = "test_data_export.csv"
-rc += call([CASE_DIR + "exportResults.sh", "-n", name])
+        # Run the test case
+        rc += call([CASE_DIR + "runAll.sh", "-y", "-p", str(NUM_CPU)])
 
+        rc += call([CASE_DIR + "exportResults.sh", "-n", self.case_name])
 
-print("done from python!")
+        return rc
 
-print("exit code:", rc)
+if __name__ == '__main__':
+
+    test_case = Case("test_data_case", 5, 100)
+    test_case.run()
+
+    sys.exit(main())  # next section explains the use of sys.exit
